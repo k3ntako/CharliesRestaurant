37320f2b465d8d213206a45ff9dbc059993c2846
Kentaro Kaneki <kentarokaneki@gmail.com>
2018-04-28 08:43:20 -0400

    Added Recipe

--- first.js
+++ first.js
@@ -30,6 +30,19 @@ function buy(id){
     console.log(machines)
   }
 }
+function make(ID){
+  for(let i = 0; i < menu["burger"].length;i++){
+    if (Number(document.getElementById(menu["ingredients"][i]).innerHTML) < menu[ID][i]){
+      return;
+    }
+  }
+  for(let i = 0; i < menu["burger"].length;i++){
+    document.getElementById(menu["ingredients"][i]).innerHTML = Number(document.getElementById(menu["ingredients"][i]).innerHTML) - menu[ID][i]
+  }
+  document.getElementById(ID).innerHTML = Number(document.getElementById(ID).innerHTML) +1
+
+}
+
 function sell (ID, price){
   if (Number(document.getElementById(ID).innerHTML) > 0){
     document.getElementById(ID).innerHTML = Number(document.getElementById(ID).innerHTML) -1
@@ -44,4 +57,10 @@ function buyGroceries(ID, cost){
     console.log(document.getElementById(ID).innerHTML)
   }
 
+}
+
+let menu = {
+  ingredients : ["i0","i1","i2","i3","i4"],
+  burger : [1,1,1,1,0],
+  cheeseBurger : [1,1,1,1,0]
 }
